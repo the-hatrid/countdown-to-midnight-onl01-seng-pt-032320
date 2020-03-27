6a149316ecfc96ac54fcf52db5d5d9530c5be290
bruce willis <fashun_vyctum@yahoo.com>
2020-03-27 04:41:37 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
-#write your code here
+def countdown(seconds)
+  
+  while seconds != 0 
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+end
+"HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(seconds)
+  
+  while seconds != 0 
+  puts "#{seconds} SECOND(S)!"
+  sleep(5)
+  seconds -= 1
+end
+"HAPPY NEW YEAR!"
 end
