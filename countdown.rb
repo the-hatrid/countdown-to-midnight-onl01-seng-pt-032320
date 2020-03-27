b72056280e93a64c88a13dfe0a188ce18c4c8bed
bruce willis <fashun_vyctum@yahoo.com>
2020-03-27 04:40:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -7,10 +7,11 @@ end
 "HAPPY NEW YEAR!"
 end
 
-def countdown(seconds)
+def countdown_with_sleep(seconds)
   
   while seconds != 0 
   puts "#{seconds} SECOND(S)!"
+  sleep(5)
   seconds -= 1
 end
 "HAPPY NEW YEAR!"
